af8cca0cf9e632886a45dec3211f7552cf79fe99
Vladimir Ryabtsev <vladimir.ryabtsev@gmail.com>
2025-11-23 18:43:50 -0800

    Add documentation and examples

---
+++ examples/export_with_query.py
@@ -0,0 +1,15 @@
+from ios_backup import Backup
+
+
+backup_path = 'tests/.data/backup_2/c577c025f51ed9e1db9b539dc23577e8861acc72'
+backup = Backup(backup_path)
+db = backup.db
+
+# Export all videos.
+query = """
+select * from Files
+where domain = 'CameraRollDomain' and relativePath like 'Media/DCIM/%.MOV'
+"""
+
+content = Backup.parse(db.buffered_query(query), parse_metadata=True)
+backup.export(content, 'tests/.data/exported_videos', restore_modified_dates=True)
---
+++ examples/iterate_over_query.py
@@ -0,0 +1,31 @@
+from ios_backup import Backup
+
+
+backup_path = 'path/to/backup/location'
+backup = Backup(backup_path)
+db = backup.db
+
+# Process all iMessage attachments.
+query = """
+select * from Files
+where domain = 'MediaDomain' and relativePath like 'Library/SMS/Attachments/%'
+  and (relativePath like '%.jpg'
+    or relativePath like '%.jpeg'
+    or relativePath like '%.heic'
+    or relativePath like '%.png'
+    or relativePath like '%.gif'
+    or relativePath like '%.tiff'
+    or relativePath like '%.psd'
+    or relativePath like '%.mov')
+"""
+rows = db.buffered_query(query)
+
+for record in Backup.parse(rows):
+    file = backup.base_path / record.content_path
+
+    # Copy or process the file as needed.
+    file.copy('destination/directory')
+
+    with file.open('rb') as f:
+        data = f.read()
+        # Do something with the data.
